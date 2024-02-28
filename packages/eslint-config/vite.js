6ce1304133810ae9e53cd412340d76c0d57ceac9
RezaHeydarii <re7798@gmail.com>
2024-02-28 23:04:21 +0330

    chore(Project ): initial start

---
+++ packages/eslint-config/vite.js
@@ -0,0 +1,16 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  env: { browser: true, es2020: true },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
